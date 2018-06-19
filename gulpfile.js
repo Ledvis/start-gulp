e57f9f2d29babd9b2622b5f3104541cef799a888
Zhenya <fedyai.eugen@gmail.com>
2018-06-19 12:01:20 +0300

    Configure gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,57 +11,73 @@ const mqpacker = require(`css-mqpacker`);
 const minify = require(`gulp-csso`);
 const rename = require(`gulp-rename`);
 const imagemin = require(`gulp-imagemin`);
+const rollup = require(`gulp-better-rollup`);
+const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`);
+const commonjs = require(`rollup-plugin-commonjs`);
 
 gulp.task(`style`, () => {
-  return gulp.src(`sass/style.scss`).
-    pipe(plumber()).
-    pipe(sass()).
-    pipe(postcss([
-      autoprefixer({
-        browsers: [
-          `last 1 version`,
-          `last 2 Chrome versions`,
-          `last 2 Firefox versions`,
-          `last 2 Opera versions`,
-          `last 2 Edge versions`
-        ]
-      }),
-      mqpacker({sort: true})
-    ])).
-    pipe(gulp.dest(`build/css`)).
-    pipe(server.stream()).
-    pipe(minify()).
-    pipe(rename(`style.min.css`)).
-    pipe(gulp.dest(`build/css`));
+  return gulp.src(`src/scss/style.scss`).
+  pipe(plumber()).
+  pipe(sass()).
+  pipe(postcss([
+    autoprefixer({
+      browsers: [
+        `last 1 version`,
+        `last 2 Chrome versions`,
+        `last 2 Firefox versions`,
+        `last 2 Opera versions`,
+        `last 2 Edge versions`
+      ]
+    }),
+    mqpacker({
+      sort: true
+    })
+  ])).
+  pipe(gulp.dest(`build/css`)).
+  pipe(server.stream()).
+  pipe(minify()).
+  pipe(rename(`style.min.css`)).
+  pipe(gulp.dest(`build/css`));
 });
 
 gulp.task(`scripts`, () => {
-  return gulp.src(`js/**/*.js`).
-    pipe(plumber()).
-    pipe(gulp.dest(`build/js/`));
+  return gulp.src(`src/js/**/*.js`).
+  pipe(plumber()).
+  pipe(sourcemaps.init()).
+  // use IIFE format
+  pipe(rollup({}, `iife`)).
+  pipe(sourcemaps.write(``)).
+  pipe(gulp.dest(`build/js/`));
 });
 
 gulp.task(`imagemin`, [`copy`], () => {
   return gulp.src(`build/img/**/*.{jpg,png,gif}`).
-    pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ])).
-    pipe(gulp.dest(`build/img`));
+  pipe(imagemin([
+    imagemin.optipng({
+      optimizationLevel: 3
+    }),
+    imagemin.jpegtran({
+      progressive: true
+    })
+  ])).
+  pipe(gulp.dest(`build/img`));
 });
 
 gulp.task(`copy-html`, () => {
-  return gulp.src(`*.{html,ico}`).
-    pipe(gulp.dest(`build`)).
-    pipe(server.stream());
+  return gulp.src(`src/*.{html,ico}`).
+  pipe(gulp.dest(`build`)).
+  pipe(server.stream());
 });
 
 gulp.task(`copy`, [`copy-html`, `scripts`, `style`], () => {
   return gulp.src([
-    `fonts/**/*.{woff,woff2}`,
-    `img/*.*`
-  ], {base: `.`}).
-    pipe(gulp.dest(`build`));
+    `src/fonts/**/*.{woff,woff2}`,
+    `src/img/*.*`
+  ], {
+    base: `./src`
+  }).
+  pipe(gulp.dest(`build`));
 });
 
 gulp.task(`clean`, () => {
@@ -82,13 +98,13 @@ gulp.task(`serve`, [`assemble`], () => {
     ui: false
   });
 
-  gulp.watch(`sass/**/*.{scss,sass}`, [`style`]);
-  gulp.watch(`*.html`).on(`change`, (e) => {
+  gulp.watch(`src/scss/**/*.{scss,sass}`, [`style`]);
+  gulp.watch(`src/*.html`).on(`change`, (e) => {
     if (e.type !== `deleted`) {
       gulp.start(`copy-html`);
     }
   });
-  gulp.watch(`js/**/*.js`, [`js-watch`]);
+  gulp.watch(`src/js/**/*.js`, [`js-watch`]);
 });
 
 gulp.task(`assemble`, [`clean`], () => {
@@ -99,5 +115,16 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, () => {
+gulp.task(`test`, function() {
+  return gulp
+    .src([`js/**/*.test.js`])
+    .pipe(rollup({
+      plugins: [
+        commonjs()
+      ]
+    }, `cjs`))
+    .pipe(gulp.dest(`build/test`))
+    .pipe(mocha({
+      reporter: `spec`
+    }));
 });
